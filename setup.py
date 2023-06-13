fad996b20f1a37a29913c92542050d98060cabde
my871432122 <871432122@qq.com>
2023-06-13 08:27:24 +0000

    modify setup.py for requirement.txt (hope to upgrade a high-level setup approach)

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import pathlib
 from distutils.core import setup
 from setuptools import find_packages
+import pkg_resources
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
 setup(
     name='reLLMs',
     version='0.1.0',
     packages=find_packages(),
     license='MIT License',
     long_description=open('README.md').read(),
+    install_requires=install_requires,
 )
